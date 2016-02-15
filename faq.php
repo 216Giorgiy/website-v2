74c82f536a78fdac1746bfa5b602f71646779fd2
Wen Jia Liu <wj32.64@gmail.com>
2016-02-15 13:20:56 +1100

    Update FAQ

--- faq.php
+++ faq.php
@@ -52,10 +52,6 @@
                     <dt>"Process Hacker"? Is this a dangerous "hacking" tool?</dt>
                     <dd>No. Please read about the <a href="http://catb.org/~esr/faqs/hacker-howto.html#what_is">correct definition of "hacker"</a>.</dd>
 
-                    <dt>Why is Process Hacker able to kill processes that no other tools can kill?</dt>
-                    <dd>Process Hacker loads a driver that searches memory for an internal Microsoft kernel function and calls it. This special function is not known to be hooked by
-                    any malware and security software.</dd>
-
                     <dt>Process Hacker can kill my anti-virus software! Is this a bug in the anti-virus software?</dt>
                     <dd>No. Please do not report these incidents as bugs because you will be wasting their time.</dd>
 
@@ -63,37 +59,35 @@
                     <dd>
                         Firstly, you need the latest <code>dbghelp.dll</code> version:<br>
                         <br>
-                        1) Install the latest Windows SDK. (links are below)<br>
-                        2) Open Process Hacker options via the main menu: Hacker &gt; Options<br>
+                        1) Install the latest Windows SDK (links are below).<br>
+                        2) Open Process Hacker options via the main menu: <strong>Hacker &gt; Options</strong><br>
                         3) Click Symbols, and locate <code>dbghelp.dll</code><br>
                         <dl>
                             <dd>
                                 Windows XP, Vista and Windows 7 SDK:<br>
                                 <code>C:\Program Files\Debugging Tools for Windows (x86)\</code><br><br>
                                 Windows 8 or above SDK:<br>
-                                32bit: <code>\Program Files (x86)\Windows Kits\8.x\Debuggers\x86\</code><br>
-                                64bit: <code>\Program Files (x86)\Windows Kits\8.x\Debuggers\x64\</code><br>
+                                32bit: <code>C:\Program Files (x86)\Windows Kits\8.x\Debuggers\x86\</code><br>
+                                64bit: <code>C:\Program Files (x86)\Windows Kits\8.x\Debuggers\x64\</code><br>
                             </dd>
                         </dl>
                         Secondly, you need to configure the search path. If you don't know what to do, enter:<br>
                         <code>SRV*SOME_FOLDER*http://msdl.microsoft.com/download/symbols</code><br><br>
                         Replace <code>SOME_FOLDER</code> with any folder you can write to, like <code>D:\Symbols</code>.
-                        Now you can restart Process Hacker and view full symbols.
+                        Now you can restart Process Hacker and see full symbols.
                     </dd>
 
                     <dt>Why can't I build Process Hacker?</dt>
                     <dd>
                         The most likely problem is that you do not have the latest Windows SDK installed.<br>
-                        Windows XP, Vista and Windows 7 SDK: <a href="http://msdn.microsoft.com/en-us/windows/bb980924.aspx">Windows SDK</a><br>
-                        Windows 7, Windows 8 SDK: <a href="http://msdn.microsoft.com/en-US/windows/hardware/hh852363">Windows 8 SDK</a><br>
-                        Windows 7, Windows 8 and 8.1 SDK: <a href="http://msdn.microsoft.com/en-US/windows/desktop/bg162891">Windows 8.1 SDK</a>
+                        Download: <a href="https://dev.windows.com/en-us/downloads/windows-10-sdk">Windows 10 SDK</a>
                     </dd>
 
                     <dt>Anti-cheat software reports Process Hacker as a game cheating tool!</dt>
-                    <dd>Unfortunately there is nothing much that can be done about this.</dd>
+                    <dd>Unfortunately there is nothing much that can be done about this. Just avoid double-clicking on memory regions, injecting DLLs or accessing handles of game processes.</dd>
 
-                    <dt>How do I report issues with Anti-cheat software?</dt>
-                    <dd>Contact <a href="about.php">dmex</a></dd>
+                    <dt>How do I report issues with anti-cheat software?</dt>
+                    <dd>Contact <a href="about.php">dmex</a>.</dd>
                 </dl>
             </div>
         </div>
