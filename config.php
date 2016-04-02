dd5d56cc3455885a5250227c591996cbd3636f18
Wen Jia Liu <wj32.64@gmail.com>
2016-04-02 14:21:16 +1100

    2.39 release

--- config.php
+++ config.php
@@ -1,21 +1,25 @@
 <?php
-$LATEST_PH_VERSION = "2.38";
-$LATEST_PH_BUILD = "343";
-$LATEST_PH_RELEASE_DATE = "26th of February 2016";
-
-$LATEST_PH_BIN_SIZE = "3.4 MB";
-$LATEST_PH_BIN_SHA1 = "f9ae9036e657393599d3282dddda4ccbb33ae11b";
-
-$LATEST_PH_SDK_SIZE = "5.0 MB";
-$LATEST_PH_SDK_SHA1 = "312e012f8a3666c5cea907cbea9c4e9a4bd73625";
+$LATEST_PH_VERSION = "2.39";
+$LATEST_PH_BUILD = "124";
+$LATEST_PH_RELEASE_DATE = "29th of March 2016";
 
 $LATEST_PH_SETUP_SIZE = "2.3 MB";
-$LATEST_PH_SETUP_SHA1 = "8953d402f80d18423e8744edb05eb6582ed11019";
+$LATEST_PH_SETUP_SHA1 = "162b08b0b11827cc024e6b2eed5887ec86339baa";
+$LATEST_PH_SETUP_SHA2 = "28042dd4a92a0033b8f1d419b9e989c5b8e32d1d2d881f5c8251d58ce35b9063";
 
-$LATEST_PH_SOURCE_SIZE = "2.7 MB";
-$LATEST_PH_SOURCE_SHA1 = "eb8b2e45335fd15e9b5e4c8093b6a383e4df115d";
+$LATEST_PH_BIN_SIZE = "3.4 MB";
+$LATEST_PH_BIN_SHA1 = "8e8f8423d163d922242b8b7d85427664f77edc97";
+$LATEST_PH_BIN_SHA2 = "2afb5303e191dde688c5626c3ee545e32e52f09da3b35b20f5e0d29a418432f5";
 
-$LATEST_PH_RELEASE_NEWS = "https://wj32.org/processhacker/forums/viewtopic.php?f=1&t=2255";
+$LATEST_PH_SDK_SIZE = "4.9 MB";
+$LATEST_PH_SDK_SHA1 = "01943f99165a8e37940dce2ba806c09034f0a62f";
+$LATEST_PH_SDK_SHA2 = "";
+
+$LATEST_PH_SOURCE_SIZE = "3.3 MB";
+$LATEST_PH_SOURCE_SHA1 = "3663ac5aea5a57d6572187b6c3d2b1066d6bbd07";
+$LATEST_PH_SOURCE_SHA2 = "ed4fe28a973d4750f02c298871af2da35b817c4d9e0d25c5e59f514eb215b75e";
+
+$LATEST_PH_RELEASE_NEWS = "https://wj32.org/processhacker/forums/viewtopic.php?f=1&t=2293";
 
 //$LATEST_PH_RELEASE_URL = "http://sourceforge.net/projects/processhacker/files/processhacker2/processhacker-".$LATEST_PH_VERSION."-setup.exe/download?use_mirror=autoselect";
 $LATEST_PH_RELEASE_URL = "https://github.com/processhacker2/processhacker2/releases/download/v".$LATEST_PH_VERSION."/processhacker-".$LATEST_PH_VERSION."-setup.exe";
